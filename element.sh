cc46275f0ee58ff46b9ac26f295ccf1fb4ab24f2
S-rupesh K <rupeshsinghremind@gmail.com>
2024-12-24 23:11:33 +0000

    feat: added PSQL connection and argumet checking condition

--- element.sh
+++ element.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only -c"
+if [ $# -eq 0 ]; then
+  echo "Please provide and element as an argument"
+else
+  echo "working"  
+fi
